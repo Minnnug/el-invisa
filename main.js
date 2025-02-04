5eb7cda2da091984b6aa5b31b8bf45affe1e05e9
Minnnug <niina.tenhunen2@edu.salpaus.fi>
2025-02-04 11:00:38 +0200

    update

---
+++ main.js
@@ -0,0 +1,43 @@
+
+const quizdata = [
+    {
+        question: "What animal is said to have nine lives?",
+        options: ["Fox","Cat", "Dog"],
+        correct: "Cat"
+    },
+    {
+        question: "What color is the tongue of a giraffe?",
+        options: ["Blue", "Green", "Purple"],
+        correct: "Purple"
+    },
+    {
+        question: "What is the only mammal that cant jump?",
+        options: ["Giraffe", "Elephant", "Whale"],
+        correct: "Elephant"
+    },
+    {
+        question: "What does a snake shed a few times a year?",
+        options: ["Its skin", "Its tail", "Its tongue"],
+        correct: "Its skin"
+    },
+    {
+        question: "Why do frogs croak?",
+        options: ["To communicate", "To attract a mate", "To mark their territory"],
+        correct: "To attract a mate"
+    },
+    {
+        question: "How many eyes do caterpillars have?",
+        options: ["8", "12", "6"],
+        correct: "12"
+    },
+    {
+        question: "How many years can a snail sleep for?",
+        options: ["8", "1", "3"],
+        correct: "3"
+    },
+    {
+        question: "What is the fastest animal on land?",
+        options: ["Lion", "Cheetah", "Panther"],
+        correct: "Cheetah"
+    }
+];
